60a323935984b61bad50e7d646ea90ca73219afa
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-03-25 14:52:32 -0400

    Updates

---
+++ precache-manifest.2aae863fc15bc2bc9a0eab97e7e2a640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7168ba1065fb85b6c2aeb9838f78500c",
+    "url": "/KnicksAssetDirectory2/index.html"
+  },
+  {
+    "revision": "2a595488ddf63259f8cb",
+    "url": "/KnicksAssetDirectory2/static/css/main.4a2a0f46.chunk.css"
+  },
+  {
+    "revision": "87fdd7b2b4eba6b8e233",
+    "url": "/KnicksAssetDirectory2/static/js/2.1bf73677.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KnicksAssetDirectory2/static/js/2.1bf73677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a595488ddf63259f8cb",
+    "url": "/KnicksAssetDirectory2/static/js/main.3474cb1f.chunk.js"
+  },
+  {
+    "revision": "d0ba953cbbb6f2e2d074",
+    "url": "/KnicksAssetDirectory2/static/js/runtime-main.3df5374f.js"
+  }
+]);
